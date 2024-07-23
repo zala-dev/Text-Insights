bf24f17d50b465c736efc7dbb35017406e600288
Zala <zala.developer@gmail.com>
2024-07-24 01:07:58 +0930

    complete text area component functionality

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -1,3 +1,40 @@
+import { useState } from "react";
+import Warning from "./Warning";
+
 export default function TextArea() {
-  return <textarea className="textarea" />;
+  const [text, setText] = useState("");
+  const [warningMessage, setWarningMessage] = useState("");
+
+  const handleOnChange = (e) => {
+    let newText = e.target.value;
+    newText = sanitizeText(newText);
+    setText(newText);
+  };
+
+  // validation
+  const sanitizeText = (text) => {
+    if (text.includes("<script>")) {
+      setWarningMessage("Script tag not allowed!");
+      text = text.replace(/<script>/gi, "");
+    } else if (text.includes("@")) {
+      setWarningMessage("No @ symbol allowed!");
+      text = text.replace(/@/gi, "");
+    } else {
+      setWarningMessage("");
+    }
+    return text;
+  };
+
+  return (
+    <div className="textarea">
+      <textarea
+        name="text"
+        value={text}
+        spellCheck="false"
+        placeholder="Enter your text here"
+        onChange={handleOnChange}
+      />
+      {warningMessage ? <Warning warningMessage={warningMessage} /> : null}
+    </div>
+  );
 }
---
+++ src/Warning.jsx
@@ -0,0 +1,3 @@
+export default function Warning({ warningMessage }) {
+  return <p className="warning">{warningMessage}</p>;
+}
