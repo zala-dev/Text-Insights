f34420529da2e9028787c77120b2c95015496372
Zala <zala.developer@gmail.com>
2024-07-24 01:25:57 +0930

    complete ui for stat component

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,3 +1,27 @@
 export default function Stats() {
-  return <section className="stats">Stats</section>;
+  const statData = [
+    { name: "Words", number: 0 },
+    { name: "Characters", number: 0 },
+    { name: "Instagram", number: 280 },
+    { name: "Facebook", number: 2200 },
+  ];
+
+  const Stat = ({ name, number }) => {
+    return (
+      <article className="stat">
+        <span className="stat__number">{number}</span>
+        <h2 className="second-heading">{name}</h2>
+      </article>
+    );
+  };
+
+  return (
+    <section className="stats">
+      {statData
+        ? statData.map((el, i) => (
+            <Stat key={i} name={el.name} number={el.number} />
+          ))
+        : ""}
+    </section>
+  );
 }
